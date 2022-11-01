27e3c63fa60332e5f882f49823eda26f07f267a2
Erik Ranby <erik.ranby@sellpy.se>
2022-11-01 16:38:48 +0100

    add tests for util function `convert`

---
+++ src/utils/index.test.js
@@ -0,0 +1,21 @@
+/* eslint-env jest */
+const { convert } = require('.')
+
+describe('correct conversion between currencies', () => {
+  test('Unsupported currencies are handled', () => {
+    expect(convert({ value: 10, currency: 'SEK' })('NOK')).toThrow('Non-supported currency')
+  })
+
+  test('Converts SEK <-> EUR correct', () => {
+    expect(convert({ value: 25, currency: 'SEK'})('EUR')).toEqual({ value: 2.5, currency: 'EUR' })
+    expect(convert({ value: 2.5, currency: 'EUR'})('SEK')).toEqual({ value: 25, currency: 'SEK' })
+  })
+  test('Converts SEK <-> DKK correct', () => {
+    expect(convert({ value: 25, currency: 'SEK'})('DKK')).toEqual({ value: 17.5, currency: 'DKK' })
+    expect(convert({ value: 17.5, currency: 'DKK'})('SEK')).toEqual({ value: 25, currency: 'SEK' })
+  })
+  test('Converts EUR <-> DKK correct', () => {
+    expect(convert({ value: 25, currency: 'EUR'})('DKK')).toEqual({ value: 192.31, currency: 'DKK' })
+    expect(convert({ value: 192.31, currency: 'DKK'})('EUR')).toEqual({ value: 25, currency: 'EUR' })
+  })
+})
