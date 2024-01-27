b86cb33b5de3410092f301255520aa3fbef837f2
Gustav Sandström <gustav.sandstroem@gmail.com>
2024-01-27 09:48:37 +0100

    moongoose hadling of list of _doc objects in postman

--- src/routes/index.js
+++ src/routes/index.js
@@ -12,7 +12,7 @@ module.exports = (app) => {
     try {
       const market = req.params.market;
       if (!isValid(market)) return res.status(400).json({ message: `market ${market} is not valid.` })
-      const results = await Item.find({})
+      const results = (await Item.find({})).map(r => r.toObject());
       const market_adjusted_results = transform(market, results)
       res.status(200).send(market_adjusted_results)
     } catch (error) {
